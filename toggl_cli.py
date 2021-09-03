4883c32baa2670051b0e1621b2ebb625ef35f558
Lucas Mundim <lucas.mundim044@gmail.com>
2021-09-03 17:59:10 -0300

    Change to use default values

--- toggl_cli.py
+++ toggl_cli.py
@@ -1,16 +1,31 @@
 import argparse
 import command_handler
+from utils import get_current_month_first_day, get_current_month_last_day
 
 parser = argparse.ArgumentParser(prog='toggl-cli', description='Work with Toggl')
 
 parser.add_argument('command', type=str, help='config, timesheet')
 
 parser.add_argument('--key', '-k', type=str, help='Your Toggl API Key', required=False)
-parser.add_argument('--start', '-s', type=str, help='Start date, YYYY-MM-DD', required=False)
-parser.add_argument('--end', '-e', type=str, help='End date, YYYY-MM-DD', required=False)
 parser.add_argument('--file', '-f', action='store_true', help='uses config file when specified', required=False)
 parser.add_argument('--json', '-j', action='store_true', help='outputs json when specified', required=False)
 parser.add_argument('--csv', '-c', action='store_true', help='outputs csv when specified', required=False)
+parser.add_argument(
+    '--start',
+    '-s',
+    type=str,
+    help='Start date, YYYY-MM-DD. If none provided will use current month day 1',
+    required=False,
+    default=str(get_current_month_first_day())
+)
+parser.add_argument(
+    '--end',
+    '-e',
+    type=str,
+    help='End date, YYYY-MM-DD. If none provided will use current month last day',
+    required=False,
+    default=str(get_current_month_last_day())
+)
 
 args = parser.parse_args()
 
--- toggl_facade.py
+++ toggl_facade.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from utils import get_token, get_current_month_first_day, get_current_month_last_day
+from utils import get_token
 from datetime import datetime
 from models.timesheet_summary import TimesheetSummary
 import toggl_api
@@ -9,11 +9,7 @@ def get_timesheet_summary(args):
 
     base64_key = toggl_api.create_authorization(api_key)
 
-    start_date = args.start if args.start is not None else str(get_current_month_first_day())
-    end_date = args.end if args.end is not None else str(get_current_month_last_day())
-    print(start_date, end_date)
-
-    entries = toggl_api.get_time_entries_from_period(start_date, end_date, base64_key)
+    entries = toggl_api.get_time_entries_from_period(args.start, args.end, base64_key)
 
     period_worked_hours = 0
     hours_per_day = dict()
@@ -29,6 +25,11 @@ def get_timesheet_summary(args):
             hours_per_day[start_date] = round(hours, 1)
         period_worked_hours = period_worked_hours + hours
 
-    response = TimesheetSummary(args.start, args.end, round(period_worked_hours, 1), hours_per_day.items())
+    response = TimesheetSummary(
+        args.start,
+        args.end,
+        round(period_worked_hours, 1),
+        hours_per_day.items()
+    )
 
     return response
